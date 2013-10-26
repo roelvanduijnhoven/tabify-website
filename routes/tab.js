26a3d4953c976a4f93436b9ea86c322764953795
Roel van Duijnhoven <roel.duijnhoven@gmail.com>
2013-10-26 23:12:49 +0200

    Make use of new version of tabify.

--- routes/tab.js
+++ routes/tab.js
@@ -3,10 +3,10 @@
 exports.upload = function(req, res){
 
     var fs = require('fs');
-    var midiParser = require('midifile');
+
     var tab = require('tabify/lib/tab');
+    var tabber = require('tabify/lib/tabber');
     var midiHelper = require('tabify/lib/midi-helper');
-    var bufferHelper = require('tabify/lib/buffer-helper');
     var guitarHelper = require('tabify/lib/guitar-helper');
 
     fs.readFile(req.files.midi.path, function (err, data) {
@@ -15,26 +15,16 @@ exports.upload = function(req, res){
             return;
         }
 
-        var bufferedMidi = bufferHelper.toArrayBuffer(data);
-        var midi = new midiParser(bufferedMidi);
-
         var guitar = guitarHelper.placeCapo(guitarHelper.standardTuning(), Number(req.body.capo));
-        var sheet = new tab.Sheet(guitar.length);
+        var sheet = new tab.Sheet(guitar);
 
-        var notes = midiHelper.getNotes(midi);
-        for (var i in notes) {
-            var note = notes[i];
-            var string = guitarHelper.bestStringToPlay(guitar, note);
+        var midi = midiHelper.createFromRaw(data);
+        tabber.usingMidi(sheet, midi);
 
-            if (!string) {
-                process.stdout.write("\nSong is unplayable using this (tuned) guitar\n\n");
-                return;
-            }
-
-            sheet.place(string, guitarHelper.fretToPlayNoteOn(guitar, string, note));
-            sheet.rest();
+        var params = {
+            bars: 78
         }
 
-        res.render('upload', {tab: tab.format(sheet, 80), capo: Number(req.body.capo)});
+        res.render('upload', {tab: tab.format(sheet, params), capo: Number(req.body.capo)});
     });
 };
